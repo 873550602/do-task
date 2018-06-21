c5b2885b036e513b7f4aaf5f2ced95e6a5e0044b
hxx <873550602@qq.com>
2018-06-22 01:28:34 +0800

    'signIn-and-index'

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,19 +1,70 @@
 <template>
-  <div class="layout">
-  	
-  </div>
+	<mu-container class="layout">
+		<mu-appbar style="width: 100%;" color="primary">
+			<mu-button icon slot="left" @click="open = !open">
+				<mu-icon value="menu"></mu-icon>
+			</mu-button>
+			收件箱
+			<mu-menu slot="right">
+				<mu-button flat>
+					<mu-icon value="more_vert"></mu-icon>
+				</mu-button>
+				<mu-list slot="content">
+					<mu-list-item button>
+						<mu-list-item-content>
+							<mu-list-item-title>Menu Item 1</mu-list-item-title>
+						</mu-list-item-content>
+					</mu-list-item>
+					<mu-list-item button>
+						<mu-list-item-content>
+							<mu-list-item-title>Menu Item 2</mu-list-item-title>
+						</mu-list-item-content>
+					</mu-list-item>
+				</mu-list>
+			</mu-menu>
+		</mu-appbar>
+		<mu-drawer :open.sync="open" :docked="docked" :right="position === 'right'">
+			<mu-row>
+				<mu-col span="12" class="sider-top">
+					<img class="photo" src="../assets/img/def_photo.png" />
+					<mu-icon size="30" color="#fff" value="search" class="right"></mu-icon>
+					<mu-flex class="flex-wrapper" justify-content="center">
+							<mu-button full-width color="#bbdefb">登录或注册</mu-button>
+					</mu-flex>
+				</mu-col>
+			</mu-row>
+		</mu-drawer>
+	</mu-container>
 </template>
 
 <script>
-export default {
-  name: 'index',
-  data () {
-    return {
-    }
-  }
-}
+	export default {
+		name: 'index',
+		data() {
+			return {
+				open: false,
+				docked: false,
+				position: 'left'
+			}
+		}
+	}
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
-
+	.sider-top {
+		padding: 30px 10px;
+		height: 150px;
+		background: #42a5f5;
+		img.photo {
+			width: 50px;
+			height: 50px;
+			vertical-align: top;
+			border-radius: 25px;
+			background: #e3f2fd;
+			border: 2px solid #fff;
+		}
+		.flex-wrapper{
+			margin-top: 20px;
+		}
+	}
 </style>
--- src/views/signIn.vue
+++ src/views/signIn.vue
@@ -2,12 +2,21 @@
 	<div class="layout">
 		<mu-container class="form-box">
 			<mu-row>
-				<mu-col span="12 logo-box">
+				<mu-col span="12" class="logo-box">
 					<img class="logo" src="../../build/logo.png" />
 					<div class="text-logo"> Do Task </div>
-					<mu-text-field v-model="UserName" icon="account_circle" placeholder="用户名"></mu-text-field>
-					<mu-text-field v-model="Password" icon="locked" placeholder="密码"></mu-text-field>
 				</mu-col>
+				<mu-col span="12">
+					<mu-text-field v-model="UserName" icon="account_circle" placeholder="用户名"></mu-text-field>
+				</mu-col>
+				<mu-col span="12">
+					<mu-text-field v-model="Password" icon="locked" placeholder="密码"></mu-text-field><br>
+				</mu-col>
+			</mu-row>
+			<mu-row justify-content="between" class="access-account">
+			    <mu-col span="4"><img src="../assets/img/qq.png"/></mu-col>
+			    <mu-col span="4"><img src="../assets/img/weixin.png"/></mu-col>
+			    <mu-col span="4"><img src="../assets/img/weibo.png"/></mu-col>
 			</mu-row>
 		</mu-container>
 	</div>
@@ -20,15 +29,29 @@
 	.layout {
 		height: 100%;
 		background: #fff;
-		.logo-box {
+		.form-box {
+			position: relative;
+			margin: 0 auto;
+			width: 80%;
+			height: 100%;
 			text-align: center;
-			img.logo {
-				width: 50px;
-				height: 50px;
+			.logo-box {
+				margin-top: 50px;
+				margin-bottom: 80px;
+				text-align: center;
+				img.logo {
+					width: 50px;
+					height: 50px;
+				}
+				.text-logo {
+					font-size: 1.5rem;
+					color: #55E3A5;
+				}
 			}
-			.text-logo {
-				font-size: 1.5rem;
-				color: #55E3A5;
+			.access-account{
+				width: 100%;
+				position: absolute;
+				bottom: 30px;
 			}
 		}
 	}
